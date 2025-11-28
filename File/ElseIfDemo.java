a53a6dd3a3b50fe640b5a06375c1537dccc48974
MEGHANA.P <meghanap670@gmail.com>
2025-11-28 11:18:43 +0530

    Clean up comments in ElseIfDemo.java
    Removed unnecessary comment about conditional statements.

--- File/ElseIfDemo.java
+++ File/ElseIfDemo.java
@@ -1,8 +1,6 @@
  class ElseIfDemo {
     public static void main(String[] args) {
         int temperature = 25;
- Programs to Demonstrate Conditional Statements in
- Java
         if (temperature > 35)
             System.out.println("It's too hot outside.");
         else if (temperature > 20)
